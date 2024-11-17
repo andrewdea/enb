239cc1f21112d9db218864e3422e74c6cff673f9
andrewdea <bobodeangelis@gmail.com>
2024-11-17 15:39:34 -0500

    initial commit

---
+++ notebook.el
@@ -0,0 +1,219 @@
+;;; notebook.el --- (WIP) support for Jupyter notebooks in Emacs     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Andrew De Angelis
+
+;; Author: Andrew De Angelis <bobodeangelis@gmail.com>
+;; Keywords: convenience, languages
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TODO
+
+;;; Code:
+
+(require 'json)
+
+;; TODO implement these functions
+(defun toggle-cell-type (&rest args)
+  "Toggle the type of current cell between markdown and code")
+
+(defun move-cell (&rest args)
+  "Move the cell up or down depending on ARGS")
+
+(defun insert-cell (&rest args)
+  "Insert a new cell")
+
+(defun delete-cell (&rest args)
+  "Delete the current cell")
+
+(defvar actions
+  ;; TODO rather than using emojis, integrate with all-the-icons, and
+  ;; provide pure-text alternatives
+  (list
+   (buttonize "[md/py]" #'toggle-cell-type)
+   (buttonize "  ⬆️  " #'move-cell 'up)
+   (buttonize "  ⬇️  " #'move-cell 'down)
+   (buttonize " ➕⬆️ " #'insert-cell 'above)
+   (buttonize " ➕⬇️ " #'insert-cell 'below)
+   (buttonize "  🗑️  " #'delete-cell))
+  "Actions to display next to a cell")
+
+(defvar formatted-actions
+  (propertize
+   (concat
+    (char-to-string ?\N{U+200F})
+    (string-join actions " ")
+    "\n")
+   'face 'minibuffer-prompt)
+  "`actions' formatted into the propertized string to insert in the buffer")
+
+(defun concat-vector-into-string (vec)
+  (cl-loop for line across vec
+           ;; do (message "line: %s" line)
+           concat line into str
+           finally return str))
+
+(defun get-cell-outputs (cell)
+  (if-let ((outputs (alist-get 'outputs cell))
+           ;; TODO find examples with multiple outputs and loop
+           ;; through them
+           (non-empty (> (length outputs) 0))
+           (first (aref outputs 0))
+           ;; TODO generalize this to all possible data types
+           (text (or (alist-get 'text first)
+                     (alist-get 'text-plain (alist-get 'data
+                                                       first)))))
+      (concat-vector-into-string text)
+    ""))
+
+(defun get-cell-contents (cell)
+  "Construct a string from CELL's source.
+Add delimiters specifying the cell's contents"
+  ;; TODO: this delimiters approach integrates well with markdown, but
+  ;; we could probably eventually transition to a better approach with
+  ;; invisible text that is parsed by tree-sitter. In particular, the
+  ;; backticks are redundant (we already have cell-delimiter
+  ;; overlays), and should be done away with
+  (let ((delimiters (if  (equal "markdown"
+                                (alist-get 'cell_type cell))
+                        '("# markdown cell\n" "\n")
+                      '("# code cell\n```python\n" "\n```\n")))
+        (source (concat-vector-into-string (alist-get 'source cell)))
+        (outputs (get-cell-outputs cell)))
+    (concat (car delimiters)
+            source
+            (cadr delimiters)
+            outputs)
+    ;; TODO output should be a special case that is displayed UNDER
+    ;; the cell is output `raw'? if so let's put in fundamental mode
+    ))
+
+;; this function is just for testing for now, not sure it'll be useful
+;; as a feature
+(defun cleanup-ovs ()
+  "Helper function for testing, removes all overlays from the current buffer"
+  (interactive)
+  (remove-overlays (point-min) (point-max)))
+
+(defun next-delimiter (i overlays)
+  "Return the start of the next overlay, or `point-max' when there's
+no next overlay"
+  (if-let ((ov (nth i overlays)))
+      (overlay-start ov)
+    (point-max)))
+
+(defun get-all-contents ()
+  "Read the contents of the current buffer and encode them into a JSON object"
+  (let* ((all-overlays (overlays-in (point-min) (point-max)))
+         (overlays (seq-filter
+                    (lambda (ov)
+                      (equal (overlay-get ov 'category) 'emacs-notebook))
+                    all-overlays)))
+    ;; (message "(length overlays): %s" (length overlays))
+    (cl-loop for i from 0 to (1- (length overlays))
+             vconcat
+             (let* ((this-ov (nth i overlays))
+                    (block (buffer-substring-no-properties
+                            (overlay-end this-ov)
+                            (next-delimiter (1+ i) overlays)))
+                    (display (overlay-get this-ov 'display))
+                    (props (get-text-property 0
+                                              'notebook-properties
+                                              display)))
+               (list
+                (if (not (string-empty-p block))
+                    `((source . ,(string-split block "\n"))
+                      ,@props)
+                  props)))
+             into cells
+             finally return
+             
+             (progn
+               ;; (message "%s"`((cells . ,cells)))
+               (json-encode `((cells . ,cells)))
+               )
+             )))
+
+(defun ipynb-save ()
+  "Save the current buffer into a ipynb file"
+  (interactive)
+  (let ((contents (get-all-contents)))
+    ;; TODO add check to ensure file hasn't been edited since last
+    ;; time
+    ;; also maybe we shouldn't go by buffer name but some other
+    ;; property that would be less liable to get edited by user/emacs
+    (find-file (concat
+                (replace-regexp-in-string "^\\*\\|\\*$" ""
+                                          (buffer-name))
+                ".ipynb"))
+    (erase-buffer)
+    (insert contents)
+    ;; (save-buffer)
+    ;; (kill-buffer)
+    ))
+
+
+(defun ipynb-render-buffer ()
+  "Render the notebook's JSON object into a markdown buffer"
+  (let* ((nb-contents (json-read-from-string
+                       (buffer-substring-no-properties
+                        (point-min)
+                        (point-max)))))
+    ;; set this buffer to JSON mode and move to the new buffer
+    (json-mode)
+    (switch-to-buffer (concat "*" (file-name-base) "*")
+                      nil 'same-window)
+    ;; (erase-buffer)
+    (goto-char (point-min))
+    ;; (message "(length of cells: %s" (length (alist-get 'cells
+    ;;                                                    nb-contents)))
+    (setq cells (alist-get 'cells
+                           nb-contents))
+
+    (cl-loop for cell across (alist-get 'cells
+                                        nb-contents)
+             do
+             (save-excursion
+               (insert (get-cell-contents cell)))
+             (let ((ov (make-overlay (point)
+                                     (search-forward-regexp "# .* cell"))))
+               (overlay-put
+                ov 'display
+                (propertize
+                 formatted-actions
+                 'notebook-properties (delq (assoc 'source cell)
+                                            cell)))
+               (overlay-put ov 'category 'emacs-notebook))
+             (goto-char (point-max))
+             )
+    (goto-char (point-min)))
+  ;; TODO figure out a way to connect `buffer-modified-p' to the
+  ;; status of the buffer and the file
+  (ipynb-mode))
+
+(define-derived-mode ipynb-mode markdown-mode "ipynb"
+  "Major mode for editing python notebooks."
+  (define-key ipynb-mode-map (kbd "C-x C-s") #'ipynb-save))
+
+(define-derived-mode json-ipynb-mode json-mode "json-ipynb"
+  "Major mode for rendering python notebooks."
+  (ipynb-render-buffer))
+
+(push '("\\.ipynb\\'" . json-ipynb-mode) auto-mode-alist)
+
+
+(provide 'notebook)
+;;; notebook.el ends here
